ba3be9a7895d4d01be20c420ba9414c0112fe18a
Pete Barber <pete.barber@gmail.com>
2012-05-25 22:07:56 +0100

    adding PH code and got formatting right

--- Labels/Test1/Program.cs
+++ Labels/Test1/Program.cs
@@ -9,7 +9,7 @@ namespace Test1
 {
     class Program
     {
-        const string docPath = @"C:\foo.docx";
+        const string docPath = @"C:\users\pete\foo.docx";
 
         static void Main(string[] args)
         {
@@ -23,23 +23,11 @@ namespace Test1
             }
         }
 
-        void MakePara(Document doc, string text, bool isBold = false, float fontSize = 11, string fontName = "Times New Roman")
-        {
-            var para = doc.Paragraphs.Add();
-
-            para.Range.Font.Bold = Convert.ToInt32(isBold);
-            para.Range.Font.Name = fontName;
-            para.Range.Font.Size = fontSize;
-
-            para.Range.Text = text;
-            para.Range.InsertParagraphAfter();
-        }
-
-        void PopCell(Range range, string text, bool isBold = false, float fontSize = 11, string fontName = "Times New Roman")
+        void PopCell(Range range, string text, bool isBold = false, float fontSize = 11, string fontName = "Times New Roman", bool first = false, bool centre = false)
         {
             Paragraph para;
 
-            if (isBold == true)
+            if (first == true)
             {
                 para = range.Paragraphs.First;
                 para.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
@@ -47,12 +35,10 @@ namespace Test1
             else
             {
                 para = range.Paragraphs.Add();
-                //range.InsertParagraphAfter();
-                //para = range.Paragraphs.Last;
                 para.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
             }
 
-            if (text == "")
+            if (text == "" || centre == true)
               para.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
 
             //para.Reset();
@@ -80,11 +66,12 @@ namespace Test1
             var pageSetup = doc.PageSetup;
 
             pageSetup.PaperSize = WdPaperSize.wdPaperA4;
-            pageSetup.LeftMargin = 0F; // 12.75F;
+            pageSetup.LeftMargin = 2F; // 12.75F;
             pageSetup.RightMargin = 0F; // 12.1F;
             pageSetup.TopMargin = 12.75F; // 72F;
-            pageSetup.BottomMargin = 0F; // 72F;
-            
+            pageSetup.BottomMargin = 5F; // 72F;
+        	pageSetup.HeaderDistance = 40F;
+        	pageSetup.FooterDistance = 36F;
 
             var range = doc.Range();
 
@@ -95,26 +82,28 @@ namespace Test1
 
             table.LeftPadding = 0.75F;
             table.RightPadding = 0.75F;
+        	table.TopPadding = 5F;
 
             //WdBuiltinStyle foo = WdBuiltinStyle.wdStyleTableLightGrid;          
             //table.set_Style(foo);
 
-            for (int row = 0; row < 6; ++row)
+            for (int row = 1; row <= 6; ++row)
                 for (int col = 1; col <= 2; ++col)
                 {
+					Console.WriteLine("Row:{0}, Col:{1}", row, col);
                     var cell = table.Cell(row, col);
 
                     cell.Range.Delete();
                     cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
 
-                    PopCell(cell.Range, "Horsell Jubilation Balloon Race", true, 12);
-                    PopCell(cell.Range, "", false, 12);
-                    PopCell(cell.Range, "Notice to the finder of this balloon:");
-                    PopCell(cell.Range, "Please go to the website www.diamondballoons.info to register your details and the location of the balloon. By doing so you will be entered into a draw for a £25 Amazon Gift Voucher. Good luck and thank you!");
-                    PopCell(cell.Range, "", false, 12);
-                    PopCell(cell.Range, "", false, 12);
-
+                    PopCell(cell.Range, "Horsell Jubilation Balloon Race", isBold:true, fontSize:12, first:true);
+                    PopCell(cell.Range, "", fontSize:12);
+                    PopCell(cell.Range, "Notice to the finder of this balloon:", isBold:true);
+                	PopCell(cell.Range, "Please go to the website www.diamondballoons.info to register your details and the location of the balloon. By doing so you will be entered into a draw for a £25 Amazon Gift Voucher. Good luck and thank you!");
+					PopCell(cell.Range, "ABCDE", false, 12, centre: true);
+					PopCell(cell.Range, "", fontSize: 12);
                 }
+
             doc.SaveAs2(docPath);
             doc.Close();
         }
