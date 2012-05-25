01ed97fa2df0b45720289566fbc58784d556171a
Pete Barber <pete.barber@gmail.com>
2012-05-25 13:40:33 +0100

    Changed to create a 2 column table rather than 2 column document

--- Labels/Test1/Program.cs
+++ Labels/Test1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace Test1
 {
     class Program
     {
+        const string docPath = @"C:\foo.docx";
+
         static void Main(string[] args)
         {
             try
@@ -32,26 +35,49 @@ namespace Test1
             para.Range.InsertParagraphAfter();
         }
 
+        void PopCell(Range range, string text, bool isBold = false, float fontSize = 11, string fontName = "Times New Roman")
+        {
+            Paragraph para;
+            
+            if (isBold == true)
+                para = range.Paragraphs.First;
+            else
+                para = range.Paragraphs.Add();
+
+            para.Range.Font.Bold = Convert.ToInt32(isBold);
+            para.Range.Font.Name = fontName;
+            para.Range.Font.Size = fontSize;
+
+            para.Range.Text = text;
+        }
+
 
         void Run()
         {
+            File.Delete(docPath);
+
             var word = new Microsoft.Office.Interop.Word.Application();
             var doc = word.Documents.Add();
 
-            doc.PageSetup.TextColumns.Add();
+            var range = doc.Range();
 
-            foreach (int n in Enumerable.Range(0, 100))
-            {
-                MakePara(doc, "Horsell Jubilation Balloon Race", true, 12);
-                MakePara(doc, "");
-                MakePara(doc, "Notice to the finder of this balloon:");
-                MakePara(doc, "Please go to the website www.diamondballoons.info to register your details and the location of the balloon. By doing so you will be entered into a draw for a £25 Amazon Gift Voucher. Good luck and thank you!");
+            var table = doc.Tables.Add(range, 6, 2);
 
-                // TODO: Vertical spacing
-            }
+            //WdBuiltinStyle foo = WdBuiltinStyle.wdStyleTableLightGrid;          
+            //table.set_Style(foo);
 
-            doc.SaveAs2(@"C:\foo.docx");
+            for (int row = 0; row < 6; ++row)
+                for (int col = 1; col <= 2; ++col)
+                {
+                    var cell = table.Cell(row, col);
 
+                    PopCell(cell.Range, "Horsell Jubilation Balloon Race", true, 12);
+                    PopCell(cell.Range, "");
+                    PopCell(cell.Range, "Notice to the finder of this balloon:");
+                    PopCell(cell.Range, "Please go to the website www.diamondballoons.info to register your details and the location of the balloon. By doing so you will be entered into a draw for a £25 Amazon Gift Voucher. Good luck and thank you!");
+
+                }
+            doc.SaveAs2(docPath);
             doc.Close();
         }
     }
